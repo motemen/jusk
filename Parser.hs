f7b971ba3fd7f2bd6ce3347d8f95fdff63e886cf
motemen <motemen@3e12a315-d43c-0410-9d47-cb5f01d91701>
2006-10-12 18:43:49 +0000

    Not using Parsec.Language.
    git-svn-id: svn+ssh://192.168.0.2/svn/public/jusk/trunk@118 3e12a315-d43c-0410-9d47-cb5f01d91701

--- Parser.hs
+++ Parser.hs
@@ -7,8 +7,6 @@
 -- Module definition {{{
 module Parser where
 import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Language
-import qualified Text.ParserCombinators.Parsec.Token as T
 import Data.Char (isDigit,digitToInt)
 import Monad
 import List
@@ -28,58 +26,241 @@ runLex p input = parse (do { whiteSpace; x <- p; eof; return x }) "" input
 -- }}}
 
 -- Lexer {{{
-lexer :: T.TokenParser ()
-lexer = T.makeTokenParser
-        $ javaStyle {
-            identStart = letter <|> oneOf "$_",
-            identLetter = alphaNum <|> oneOf "$_",
-            reservedNames = [
-                "break",  "else",  "new",  "var", 
-                "case",  "finally",  "return",  "void", 
-                "catch",  "for",  "switch",  "while", 
-                "continue",  "function",  "this",  "with", 
-                "default",  "if",  "throw", 
-                "delete",  "in",  "try", 
-                "do",  "instanceof",  "typeof", 
-                -- Future reserved
-                "abstract",  "enum",  "int",  "short", 
-                "boolean",  "export",  "interface",  "static", 
-                "byte",  "extends",  "long",  "super", 
-                "char",  "final",  "native",  "synchronized", 
-                "class",  "float",  "package",  "throws", 
-                "const",  "goto",  "private",  "transient", 
-                "debugger",  "implements",  "protected",  "volatile", 
-                "double",  "import",  "public"
-            ],
-            reservedOpNames = [
-                "!", "!=", "!==", "%", "%=", "&", "&&", "&&=",
-                "&=", "(", ")", "*", "*=", "+", "++", "+=",
-                ",", "-", "--", "-=", ".", "...", "/", "/=",
-                ":", "::", ";", "<", "<<", "<<=", "<=", "=",
-                "==", "===", ">", ">=", ">>", ">>=", ">>>",
-                ">>>=", "?", "[", "]", "^", "^=", "^^", "^^=",
-                "{", "|", "|=", "||", "||=", "}", "~"
-            ],
-            caseSensitive = True
-        }
+reservedNames,reservedOpNames :: [String]
+reservedNames = [
+        "break",  "else",  "new",  "var", 
+        "case",  "finally",  "return",  "void", 
+        "catch",  "for",  "switch",  "while", 
+        "continue",  "function",  "this",  "with", 
+        "default",  "if",  "throw", 
+        "delete",  "in",  "try", 
+        "do",  "instanceof",  "typeof", 
+        -- Future reserved
+        "abstract",  "enum",  "int",  "short", 
+        "boolean",  "export",  "interface",  "static", 
+        "byte",  "extends",  "long",  "super", 
+        "char",  "final",  "native",  "synchronized", 
+        "class",  "float",  "package",  "throws", 
+        "const",  "goto",  "private",  "transient", 
+        "debugger",  "implements",  "protected",  "volatile", 
+        "double",  "import",  "public"
+    ]
 
-lexeme         = T.lexeme lexer
-whiteSpace     = T.whiteSpace lexer
-symbol         = T.symbol lexer
-natural        = T.natural lexer
-float          = T.float lexer
-naturalOrFloat = T.naturalOrFloat lexer
-parens         = T.parens lexer             -- ()
-braces         = T.braces lexer             -- {}
-squares        = T.squares lexer            -- []
-colon          = T.colon lexer              -- :
-semi           = T.semi lexer               -- ;
-comma          = T.comma lexer              -- ,
-reserved       = T.reserved lexer           
-reservedOp     = T.reservedOp lexer
-hexadecimal    = T.hexadecimal lexer
-decimal        = T.decimal lexer
-identifierString = T.identifier lexer
+reservedOpNames = [
+        "!", "!=", "!==", "%", "%=", "&", "&&", "&&=",
+        "&=", "(", ")", "*", "*=", "+", "++", "+=",
+        ",", "-", "--", "-=", ".", "...", "/", "/=",
+        ":", "::", ";", "<", "<<", "<<=", "<=", "=",
+        "==", "===", ">", ">=", ">>", ">>=", ">>>",
+        ">>>=", "?", "[", "]", "^", "^=", "^^", "^^=",
+        "{", "|", "|=", "||", "||=", "}", "~"
+    ]
+
+lexeme :: Parser a -> Parser a
+lexeme p =
+    do { x <- p; whiteSpace; return x }
+
+whiteSpace :: Parser ()
+whiteSpace =
+    skipMany (simpleSpace <|> oneLineComment <|> multiLineComment <?> "")
+
+simpleSpace :: Parser ()
+simpleSpace =
+    skipMany1 (oneOf " \t") -- TODO: Unicode char
+
+oneLineComment :: Parser ()
+oneLineComment =
+    do try $ string "//"
+       skipMany $ satisfy (/= '\n')
+       return ()
+
+multiLineComment :: Parser ()
+multiLineComment =
+    do try $ string "/*"
+       inComment
+
+inComment :: Parser ()
+inComment = do { try $ string "*/"      ; return () }
+        <|> do { skipMany1 $ noneOf "*/"; inComment }
+        <|> do { oneOf "*/"             ; inComment }
+        <?> "end of comment"
+
+symbol :: String -> Parser String
+symbol name =
+    lexeme (string name)
+
+naturalOrFloat :: Parser (Either Integer Double)
+naturalOrFloat  = lexeme (natFloat) <?> "number"
+
+float           = lexeme floating   <?> "float"
+integer         = lexeme int        <?> "integer"
+natural         = lexeme nat        <?> "natural"
+
+
+-- floats
+floating        = do{ n <- decimal 
+                    ; fractExponent n
+                    }
+
+natFloat        = do{ char '0'
+                    ; zeroNumFloat
+                    }
+                  <|> decimalFloat
+                  
+zeroNumFloat    =  do{ n <- hexadecimal <|> octal
+                     ; return (Left n)
+                     }
+                <|> decimalFloat
+                <|> fractFloat 0
+                <|> return (Left 0)                  
+                  
+decimalFloat    = do{ n <- decimal
+                    ; option (Left n) 
+                             (fractFloat n)
+                    }
+
+fractFloat n    = do{ f <- fractExponent n
+                    ; return (Right f)
+                    }
+                    
+fractExponent n = do{ fract <- fraction
+                    ; expo  <- option 1.0 exponent'
+                    ; return ((fromInteger n + fract)*expo)
+                    }
+                <|>
+                  do{ expo <- exponent'
+                    ; return ((fromInteger n)*expo)
+                    }
+
+fraction        = do{ char '.'
+                    ; digits <- many1 digit <?> "fraction"
+                    ; return (foldr op 0.0 digits)
+                    }
+                  <?> "fraction"
+                where
+                  op d f    = (f + fromIntegral (digitToInt d))/10.0
+                    
+exponent'       = do{ oneOf "eE"
+                    ; f <- sign
+                    ; e <- decimal <?> "exponent"
+                    ; return (power (f e))
+                    }
+                  <?> "exponent"
+                where
+                   power e  | e < 0      = 1.0/power(-e)
+                            | otherwise  = fromInteger (10^e)
+
+
+-- integers and naturals
+int             = do{ f <- lexeme sign
+                    ; n <- nat
+                    ; return (f n)
+                    }
+                    
+-- sign            :: CharParser st (Integer -> Integer)
+sign            =   (char '-' >> return negate) 
+                <|> (char '+' >> return id)     
+                <|> return id
+
+nat             = zeroNumber <|> decimal
+    
+zeroNumber      = do{ char '0'
+                    ; hexadecimal <|> octal <|> decimal <|> return 0
+                    }
+                  <?> ""       
+
+decimal         = number 10 digit        
+hexadecimal     = do{ oneOf "xX"; number 16 hexDigit }
+octal           = do{ oneOf "oO"; number 8 octDigit  }
+
+-- number :: Integer -> CharParser st Char -> CharParser st Integer
+number base baseDigit
+    = do{ digits <- many1 baseDigit
+        ; let n = foldl (\x d -> base*x + toInteger (digitToInt d)) 0 digits
+        ; seq n (return n)
+        }          
+
+-----------------------------------------------------------
+-- Operators & reserved ops
+-----------------------------------------------------------
+reservedOp name =   
+    lexeme $ try $
+    do{ string name
+      ; notFollowedBy (oneOf "+-*/%?:&|^<>") <?> ("end of " ++ show name)
+      }
+
+{-
+operator =
+    lexeme $ try $
+    do{ name <- oper
+      ; if (isReservedOp name)
+         then unexpected ("reserved operator " ++ show name)
+         else return name
+      }
+      
+oper =
+    do{ c <- (opStart languageDef)
+      ; cs <- many (opLetter languageDef)
+      ; return (c:cs)
+      }
+    <?> "operator"
+-}
+    
+isReservedOp name =
+    isReserved (sort (reservedOpNames)) name          
+    
+    
+-----------------------------------------------------------
+-- Identifiers & Reserved words
+-----------------------------------------------------------
+reserved name =
+    lexeme $ try $
+    do{ string name
+      ; notFollowedBy (alphaNum <|> oneOf "$_") <?> ("end of " ++ show name)
+      }
+
+identifierString =
+    lexeme $ try $
+    do{ name <- ident
+      ; if (isReservedName name)
+         then unexpected ("reserved word " ++ show name)
+         else return name
+      }
+    
+    
+ident           
+    = do{ c <- (letter <|> oneOf "$_")
+        ; cs <- many (alphaNum <|> oneOf "$_")
+        ; return (c:cs)
+        }
+    <?> "identifier"
+
+isReservedName name
+    = isReserved theReservedNames name
+    
+isReserved names name    
+    = scan names
+    where
+      scan []       = False
+      scan (r:rs)   = case (compare r name) of
+                        LT  -> scan rs
+                        EQ  -> True
+                        GT  -> False
+
+theReservedNames
+    = sort reservedNames
+
+parens p        = between (symbol "(") (symbol ")") p
+braces p        = between (symbol "{") (symbol "}") p
+angles p        = between (symbol "<") (symbol ">") p
+brackets p      = between (symbol "[") (symbol "]") p
+squares         = brackets
+
+semi            = symbol ";" 
+comma           = symbol ","
+dot             = symbol "."
+colon           = symbol ":"
+
 -- }}}
 
 -- Utilities {{{
